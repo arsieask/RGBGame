4ed04aab810b35241370a2887a5b97c127f132f4
Arsen Askandaryan <arsieask@gmail.com>
2020-05-15 17:14:01 -0400

    Update

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ let colors = [
     "rgb(255, 0, 255)"
 ]
 let squares = document.querySelectorAll(".square");
-let pickedColor = colors[3];
+let pickedColor = pickColor();
 let colorDisplay = document.getElementById("colorDisplay");
-colorDisplay.textContent = pickedColor;
+let messageDisplay = document.querySelector("#message")
 
 for (let i = 0; i < squares.length; i++) {
     //add initial colors to squares
@@ -20,11 +20,25 @@ for (let i = 0; i < squares.length; i++) {
         //Grab color of clicked square
         let clickedColor =  this.style.backgroundColor;
         if (clickedColor === pickedColor){
-            alert('true')
+            messageDisplay.textContent = "Correct!";
+            changeColors(clickedColor);
         } else {
-            alert ('wrong')
+            this.style.backgroundColor = "#232323";
+            messageDisplay.textContent = "Try again"
         }
 
         //Compare color to picked color
     })
+}
+
+function changeColors(color) {
+    //Loop through all squares
+    for (let i = 0; i < squares.length; i++) {
+        squares[i].style.backgroundColor = color;
+    }
+}
+
+function pickColor() {
+    var random= Math.floor(Math.random() * colors.length);
+    return colors[random];
 }
