75946ed6cf26fa2d0958b68badec5b368a339217
Arsen Askandaryan <arsieask@gmail.com>
2020-05-18 11:21:08 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,11 @@
-let colors = [
-    "rgb(255, 0, 0)",
-    "rgb(255, 255, 0)",
-    "rgb(0, 255, 0)",
-    "rgb(0, 255, 255)",
-    "rgb(0, 0, 255)",
-    "rgb(255, 0, 255)"
-]
+let colors = generateRandomColors(6);
 let squares = document.querySelectorAll(".square");
 let pickedColor = pickColor();
 let colorDisplay = document.getElementById("colorDisplay");
-let messageDisplay = document.querySelector("#message")
+let messageDisplay = document.querySelector("#message");
+let h1 = document.querySelector("h1");
+
+colorDisplay.textContent = pickedColor;
 
 for (let i = 0; i < squares.length; i++) {
     //add initial colors to squares
@@ -18,17 +14,19 @@ for (let i = 0; i < squares.length; i++) {
     //add click listeners to squares
     squares[i].addEventListener("click", function() {
         //Grab color of clicked square
-        let clickedColor =  this.style.backgroundColor;
+        let clickedColor = this.style.backgroundColor;
+
         if (clickedColor === pickedColor){
             messageDisplay.textContent = "Correct!";
             changeColors(clickedColor);
+            h1.style.backgroundColor = clickedColor;
         } else {
             this.style.backgroundColor = "#232323";
             messageDisplay.textContent = "Try again"
         }
 
         //Compare color to picked color
-    })
+    });
 }
 
 function changeColors(color) {
@@ -39,6 +37,31 @@ function changeColors(color) {
 }
 
 function pickColor() {
-    var random= Math.floor(Math.random() * colors.length);
+    var random = Math.floor(Math.random() * colors.length);
     return colors[random];
+}
+
+function generateRandomColors(num) {
+    //Make an array
+    let arr = []
+    //Add random number to the array
+    for(let i = 0; i < num; i ++) {
+        arr.push(randomColor())
+    }
+
+    //Aeturn the array
+    return arr;
+}
+
+function randomColor() {
+    //Pick a red 0 - 255
+    let r = Math.floor(Math.random() * 256);
+
+    //Pick a green 0 - 255
+    let g = Math.floor(Math.random() * 256);
+
+    //Pick a blue 0 - 255
+    let b = Math.floor(Math.random() * 256);
+
+    return "rgb(" + r + ", " + g + ", " + b + ")";
 }
